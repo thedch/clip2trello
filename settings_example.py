e6aa9749d089caaa01b0fd0f2a5d6d6cb0475976
Daniel Hunter <officialdanielhunter@gmail.com>
2017-10-06 13:03:49 -0700

    Add board name, list name, paste path

--- settings_example.py
+++ settings_example.py
@@ -1,6 +1,19 @@
-# copy this file and name it settings.py
-# paste your key and token in there.
-# See demo.py for how to get a key and token
+# File to hold user config options
+
+# To start, in your browser, log in as the account you want to use aginst the
+# Trello API. Go to: https://trello.com/1/appKey/generate and copy the key here
+# That page should also prompt you to create a token, please do so and paste it
+# here
 
 trello_key = ''
 trello_token = ''
+
+# This is the path that the paste utility is storing files at. It's the user's
+# choice -- I recommend using the same path as this file for simplicity.
+paste_path = ''
+
+# Name of desired Trello board to place cards in. Case sensitive.
+board_name = ''
+
+# Name of desired Trello board to place cards in. Case sensitive.
+list_name = ''
