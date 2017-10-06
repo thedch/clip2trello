bf34ec5505ac3faede3962ae6d47d326ea904522
Daniel Hunter <officialdanielhunter@gmail.com>
2017-10-06 13:03:00 -0700

    Major cleanup, add more external settings, card title is now read from file

--- main.py
+++ main.py
@@ -1,52 +1,48 @@
 import requests           # a 3rd party library. I installed via `pip install requests`
 import json               # all responses from Trello come back as JSON, so have your favorite JSON library handy
 from pprint import pprint # this is a handy utility for printing dictionaries in a human readable way
+import settings
 
-
-key = ''
-# if you're storing your key in this file, you don't need the following three lines
-if not key:
-  from settings import trello_key
-  key = trello_key
-
-token = ''
-# if you're storing your token in this file, you don't need the following three lines
-if not token:
-  from settings import trello_token
-  token = trello_token
+key = settings.trello_key
+token = settings.trello_token
+board_name = settings.board_name
+list_name = settings.list_name
 
 base = 'https://trello.com/1/'
 
-# Build out the URL based on the documentation
+# Create the URL based on the documentation
 boards_url = base + 'members/me/boards'
 
-# Let's store our API key and token as parameters
+# Store the API key and token as parameters
 params_key_and_token = {'key':key,'token':token}
 
-# Since we only want the name of the board, let's supply the 'fields' argument as well. We're also going to
-# ask for lists, to be used later.
+# Since we only want the name of the board, let's supply the 'fields' argument
+# as well. We're also going to ask for lists, to be used later.
 arguments = {'fields': 'name', 'lists': 'all'}
 
-# The requests library has separate methods for get, put, post, and delete. We need a GET here.
-# We need to provide the URL we want to access, the key and token (params_key_and_token) as params, as well as
-# any arguments (arguments) as data.
 response = requests.get(boards_url, params=params_key_and_token, data=arguments)
 
 # The following will give us an array of dictionaries
 response_array_of_dict = response.json()
-# print response_array_of_dict
-# Let's go ahead and iterate through the list of boards and print the name of each board
+
+# Find the ID of the desired board
 for board in response_array_of_dict:
-  if board['name'] == 'Welcome Board':
+  if board['name'] == board_name:
     board_id = board['id']
 
 lists_url = base + 'boards/' + board_id + '/lists'
 response = requests.get(lists_url, params=params_key_and_token)
 response_array_of_dict = response.json()
-list_id = response_array_of_dict[0]['id']
 
-# TODO Create the card based on data from a file
-name = 'Card via the API'
+# Find the ID of the desired list
+for trello_list in response_array_of_dict:
+  if trello_list['name'] == list_name:
+    list_id = trello_list['id']
+
+file = open('paste.txt', 'r')
+name = file.read()
+
+# name = 'Card via the API'
 description = 'I made this card using the Trello API :fist:'
 cards_url = base + 'cards'
 
@@ -55,5 +51,3 @@ arguments = {'name': name,
              'idList' : list_id}
 
 response = requests.post(cards_url, params=params_key_and_token, data=arguments)
-
-pprint(response.json())
