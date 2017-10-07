57b0c995318bed3a26a5817f34e9f576fa3588e4
Daniel Hunter <officialdanielhunter@gmail.com>
2017-10-07 08:33:02 -0700

    Refactored to support Zapier webhooks instead of direct to Trello

--- main.py
+++ main.py
@@ -1,53 +1,15 @@
 import requests           # a 3rd party library. I installed via `pip install requests`
 import json               # all responses from Trello come back as JSON, so have your favorite JSON library handy
-from pprint import pprint # this is a handy utility for printing dictionaries in a human readable way
 import settings
 import os
 import sys
 
-key = settings.trello_key
-token = settings.trello_token
-board_name = settings.board_name
-list_name = settings.list_name
-
-base = 'https://trello.com/1/'
-
-# Create the URL based on the documentation
-boards_url = base + 'members/me/boards'
-
-# Store the API key and token as parameters
-params_key_and_token = {'key':key,'token':token}
-
-# Since we only want the name of the board, let's supply the 'fields' argument
-# as well. We're also going to ask for lists, to be used later.
-arguments = {'fields': 'name', 'lists': 'all'}
-
-response = requests.get(boards_url, params=params_key_and_token, data=arguments)
-
-# The following will give us an array of dictionaries
-response_array_of_dict = response.json()
-
-# Find the ID of the desired board
-for board in response_array_of_dict:
-  if board['name'] == board_name:
-    board_id = board['id']
-
-lists_url = base + 'boards/' + board_id + '/lists'
-response = requests.get(lists_url, params=params_key_and_token)
-response_array_of_dict = response.json()
-
-# Find the ID of the desired list
-for trello_list in response_array_of_dict:
-  if trello_list['name'] == list_name:
-    list_id = trello_list['id']
+url = settings.webooks_url
 
 # Absolute path to avoid relative issues
 file = open(os.path.join(os.path.dirname(sys.argv[0]), 'paste.txt'), 'r')
-name = file.read()
+copied_data = file.read()
 
-cards_url = base + 'cards'
+payload = {'text': copied_data}
 
-arguments = {'name': name,             
-             'idList' : list_id}
-
-response = requests.post(cards_url, params=params_key_and_token, data=arguments)
+requests.post(url, data=json.dumps(payload))
--- settings_example.py
+++ settings_example.py
@@ -1,15 +1,2 @@
-# File to hold user config options
-
-# To start, in your browser, log in as the account you want to use aginst the
-# Trello API. Go to: https://trello.com/1/appKey/generate and copy the key here
-# That page should also prompt you to create a token, please do so and paste it
-# here
-
-trello_key = ''
-trello_token = ''
-
-# Name of desired Trello board to place cards in. Case sensitive.
-board_name = ''
-
-# Name of desired Trello list to place cards in. Case sensitive.
-list_name = ''
+# The url provided by Zapier
+webooks_url = ''
